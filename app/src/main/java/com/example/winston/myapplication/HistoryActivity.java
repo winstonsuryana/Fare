fdcab440cf90145c2885d600c8eb84fadfcefc70
moomoo101 <danielstenbro101@hotmail.com>
2016-10-09 16:19:38 -0500

    Implemented History cardView
    * Functionality to Implemented card view with default data.
    * added placeholders for database values

---
+++ app/src/main/java/com/example/winston/myapplication/HistoryActivity.java
@@ -0,0 +1,199 @@
+package com.example.winston.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.style.TtsSpan;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+
+/*
+    Sample code to implement recycler view for card list: http://www.truiton.com/2015/03/android-cardview-example/
+    also taken from: https://developer.android.com/training/material/lists-cards.html#CardView
+ */
+
+
+
+public class HistoryActivity extends AppCompatActivity{
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private static String LOG_TAG = "CardViewActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new HistoryCardAdapter(getDataSet());
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Code to Add an item with default animation
+        //((MyRecyclerViewAdapter) mAdapter).addItem(obj, index);
+
+        // Code to remove an item with default animation
+        //((MyRecyclerViewAdapter) mAdapter).deleteItem(index);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ((HistoryCardAdapter) mAdapter).setOnItemClickListener(new HistoryCardAdapter
+                .MyClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Log.i(LOG_TAG, " Clicked on Item " + position);
+            }
+        });
+    }
+
+    private ArrayList<DataObject> getDataSet() {
+        ArrayList results = new ArrayList<DataObject>();
+        for (int index = 0; index < 20; index++) {
+            String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+            Float amount = new Float(0.00);
+            DataObject obj = new DataObject("Resturant " + index , "John Doe", amount, date);
+            results.add(index, obj);
+        }
+        return results;
+    }
+}
+
+class HistoryCardAdapter extends RecyclerView.Adapter<HistoryCardAdapter.DataObjectHolder> {
+    private static String LOG_TAG = "HistoryCardAdapter";
+    private ArrayList<DataObject> mDataset;
+    private static MyClickListener myClickListener;
+
+    public static class DataObjectHolder extends RecyclerView.ViewHolder
+            implements View
+            .OnClickListener {
+        TextView label;
+        TextView dateTime;
+
+        public DataObjectHolder(View itemView) {
+            super(itemView);
+            label = (TextView) itemView.findViewById(R.id.textView);
+            dateTime = (TextView) itemView.findViewById(R.id.textView2);
+            Log.i(LOG_TAG, "Adding Listener");
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            myClickListener.onItemClick(getAdapterPosition(), v);
+        }
+    }
+
+    public void setOnItemClickListener(MyClickListener myClickListener) {
+        this.myClickListener = myClickListener;
+    }
+
+    public HistoryCardAdapter(ArrayList<DataObject> myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public DataObjectHolder onCreateViewHolder(ViewGroup parent,
+                                               int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_view_row, parent, false);
+
+        DataObjectHolder dataObjectHolder = new DataObjectHolder(view);
+        return dataObjectHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(DataObjectHolder holder, int position) {
+        holder.label.setText(mDataset.get(position).getResturantName());
+        holder.dateTime.setText(mDataset.get(position).getPaidBy());
+        holder.dateTime.setText(mDataset.get(position).getDateValue());
+        //holder.label.setText((int) mDataset.get(position).getAmount());
+        //holder.dateTime.setText(mDataset.get(position).getPaidBy());
+    }
+
+    public void addItem(DataObject dataObj, int index) {
+        mDataset.add(index, dataObj);
+        notifyItemInserted(index);
+    }
+
+    public void deleteItem(int index) {
+        mDataset.remove(index);
+        notifyItemRemoved(index);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public interface MyClickListener {
+        public void onItemClick(int position, View v);
+    }
+}
+
+/*
+    Database information can be retrieved in the future using the code below.
+ */
+class DataObject {
+    public String resturantName;
+    public String paidBy;
+    public float amount;
+    public String dateValue;
+
+    DataObject (String Resturant, String paidBy, float amount, String dateValue){
+        resturantName = Resturant;
+        paidBy = paidBy;
+        amount = amount;
+        dateValue = dateValue;
+
+    }
+
+    public void setResturantName(String resturantName) {
+        this.resturantName = resturantName;
+    }
+
+    public void setPaidby(String paidBy){
+        this.paidBy = paidBy;
+
+    }
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+    public void setDateValue(String dateValue){
+        this.dateValue = dateValue;
+    }
+    public String getResturantName(){
+        return resturantName;
+    }
+    public String getPaidBy() {
+        return paidBy;
+    }
+
+    public String getDateValue(){
+        return dateValue;
+
+    }
+    public float getAmount(){
+        return amount;
+    }
+}
--- app/src/main/java/com/example/winston/myapplication/HomeScreenActivity.java
+++ app/src/main/java/com/example/winston/myapplication/HomeScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.winston.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -52,8 +53,10 @@ public class HomeScreenActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Snackbar.make(view, "HistoryActivity...", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Intent historyIntent = new Intent(HomeScreenActivity.this, HistoryActivity.class);
+                startActivity(historyIntent);
             }
         });
         /*
